1e846f8ff87d8fadd4c957607d63e6bca17bf514
정우찬 <vdncks25@naver.com>
2019-05-23 22:34:12 +0900

    FEAT : Create queries with arguments

--- graphql/db.js
+++ graphql/db.js
@@ -1,32 +1,37 @@
 export const people = [
     {
-        id: "0",
+        id: 0,
         name: "Woochan",
         age: 26, 
         gender: "male"
     }, 
     {
-        id: "1",
+        id: 1,
         name: "Duhyeon", 
         age: 27, 
         gender: "male"
     }, 
     {
-        id: "2",
+        id: 2,
         name: "hyeonghwan",
         age: 28,
         gender: "male"
     }, 
     {
-        id: "3",
+        id: 3,
         name: "Jonghyeong",
         age: 32, 
         gender: "male"
     }, 
     {
-        id: "4",
+        id: 4,
         name: "Seunggyun",
         age: 32,
         gender: "male"
     }
 ];
+
+export const getById = id => {
+    const filteredPeople = people.filter(person => person.id === id);
+    return filteredPeople[0];
+}
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,9 @@
-import { people } from "./db";
+import { people, getById } from "./db";
 
 const resolvers = {
     Query: {
         people: () => people,
+        person: (_, { id }) => getById(id)
     }
 };
 
