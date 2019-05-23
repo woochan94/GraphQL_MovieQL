678c4b9e9466e42933da859f606f3ec14bd07425
정우찬 <vdncks25@naver.com>
2019-05-23 22:06:57 +0900

    FEAT : Extend schema

---
+++ graphql/db.js
@@ -0,0 +1,32 @@
+export const people = [
+    {
+        id: "0",
+        name: "Woochan",
+        age: 26, 
+        gender: "male"
+    }, 
+    {
+        id: "1",
+        name: "Duhyeon", 
+        age: 27, 
+        gender: "male"
+    }, 
+    {
+        id: "2",
+        name: "hyeonghwan",
+        age: 28,
+        gender: "male"
+    }, 
+    {
+        id: "3",
+        name: "Jonghyeong",
+        age: 32, 
+        gender: "male"
+    }, 
+    {
+        id: "4",
+        name: "Seunggyun",
+        age: 32,
+        gender: "male"
+    }
+];
