30a21b2cc4731fc3e0dd5ce6efe655d6b7c26da9
정우찬 <vdncks25@naver.com>
2019-05-24 13:51:35 +0900

    FEAT : Create mutation
    db를 movie 관련으로 변경, 
    addMovie라는 mutation 만들고 적용

--- graphql/db.js
+++ graphql/db.js
@@ -1,37 +1,56 @@
-export const people = [
+let movies = [
     {
         id: 0,
-        name: "Woochan",
-        age: 26, 
-        gender: "male"
-    }, 
+        title: "IronMan",
+    },
     {
         id: 1,
-        name: "Duhyeon", 
-        age: 27, 
-        gender: "male"
-    }, 
+        title: "SpiderMan",
+    },
     {
         id: 2,
-        name: "hyeonghwan",
-        age: 28,
-        gender: "male"
-    }, 
+        title: "Black panther",
+    },
     {
         id: 3,
-        name: "Jonghyeong",
-        age: 32, 
-        gender: "male"
-    }, 
+        title: "Thor",
+    },
     {
         id: 4,
-        name: "Seunggyun",
-        age: 32,
-        gender: "male"
+        title: "Captain America",
+    },
+    {
+        id: 5,
+        title: "Captain Marvel",
+    },
+    {
+        id: 6,
+        title: "Avengers",
     }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === id);
-    return filteredPeople[0];
+    const filteredMovies = movies.filter(movie => movie.id === id);
+    return filteredMovies[0];
+}
+
+export const deleteMovie = (id) => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if(movies.length > cleanedMovies.length) {
+        movie = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export const addMovie = (title) => {
+    const newMovie = {
+        id: `${movies.length + 1}`, 
+        title
+    };
+    movies.push(newMovie); 
+    return newMovie;
 }
