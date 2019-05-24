d2a769b67b6542e8604f2652b29548dbbb825c9f
정우찬 <vdncks25@naver.com>
2019-05-24 16:21:23 +0900

    FEAT : Wrapping a REST API with GraphQL

--- graphql/db.js
+++ graphql/db.js
@@ -1,56 +1,46 @@
-let movies = [
-    {
-        id: 0,
-        title: "IronMan",
-    },
-    {
-        id: 1,
-        title: "SpiderMan",
-    },
-    {
-        id: 2,
-        title: "Black panther",
-    },
-    {
-        id: 3,
-        title: "Thor",
-    },
-    {
-        id: 4,
-        title: "Captain America",
-    },
-    {
-        id: 5,
-        title: "Captain Marvel",
-    },
-    {
-        id: 6,
-        title: "Avengers",
-    }
-];
+import axios from "axios"; 
 
-export const getMovies = () => movies;
+const BASE_URL = "https://yts.am/api/v2/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
-}
-
-export const deleteMovie = (id) => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if(movies.length > cleanedMovies.length) {
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
-    }
-}
-
-export const addMovie = (title) => {
-    const newMovie = {
-        id: `${movies.length + 1}`, 
-        title
-    };
-    movies.push(newMovie); 
-    return newMovie;
-}
+export const getMovies = async (limit, rating) => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios(LIST_MOVIES_URL, {
+      params: {
+        limit,
+        minimum_rating: rating
+      }
+    });
+    return movies;
+  };
+  
+  export const getMovie = async id => {
+    const {
+      data: {
+        data: { movie }
+      }
+    } = await axios(MOVIE_DETAILS_URL, {
+      params: {
+        movie_id: id
+      }
+    });
+    return movie;
+  };
+  
+  export const getSuggestions = async id => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios(MOVIE_SUGGESTIONS_URL, {
+      params: {
+        movie_id: id
+      }
+    });
+    return movies;
+  };
